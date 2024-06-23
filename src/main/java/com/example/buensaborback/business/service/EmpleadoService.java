6ff887a1230d0ce0918c6e7070641c89936a1f33
Ariel Enferrel <enferrelariel@hotmail.com>
2024-06-22 21:04:04 -0300

    agregado endpoint para buscar empleados por sucursal

--- src/main/java/com/example/buensaborback/business/service/EmpleadoService.java
+++ src/main/java/com/example/buensaborback/business/service/EmpleadoService.java
@@ -3,6 +3,7 @@ package com.example.buensaborback.business.service;
 import com.example.buensaborback.business.service.Base.BaseService;
 import com.example.buensaborback.domain.entities.Empleado;
 import com.example.buensaborback.domain.enums.Rol;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,5 +11,5 @@ public interface EmpleadoService extends BaseService<Empleado,Long> {
 
     Empleado findByEmail(String email);
     int contarPorRol(Rol rol);
-    List<Empleado> findAllBySucursalId(Long id);
+    List<Empleado> findAllBySucursalId(Long idSucursal);
 }
--- src/main/java/com/example/buensaborback/repositories/EmpleadoRepository.java
+++ src/main/java/com/example/buensaborback/repositories/EmpleadoRepository.java
@@ -2,6 +2,8 @@ package com.example.buensaborback.repositories;
 
 import com.example.buensaborback.domain.entities.Empleado;
 import com.example.buensaborback.domain.enums.Rol;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,6 @@ public interface EmpleadoRepository extends BaseRepository<Empleado, Long>{
 
     Empleado findByEmail(String email);
     int countByRol(Rol rol);
-    List<Empleado> findAllBySucursalId(Long id);
+    @Query("SELECT e FROM Empleado e JOIN e.sucursal s WHERE s.id=:idSucursal AND e.eliminado=FALSE ")
+    List<Empleado> findAllBySucursalId(@Param("idSucursal")Long idSucursal);
 }
