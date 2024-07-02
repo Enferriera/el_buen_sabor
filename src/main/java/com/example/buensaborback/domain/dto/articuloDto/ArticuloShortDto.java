6accfc538e5269765b637a37a2e50d074fa949e3
Ariel Enferrel <enferrelariel@hotmail.com>
2024-07-02 13:31:53 -0300

    corregido dto de promocionDto

--- src/main/java/com/example/buensaborback/domain/dto/articuloDto/ArticuloShortDto.java
+++ src/main/java/com/example/buensaborback/domain/dto/articuloDto/ArticuloShortDto.java
@@ -1,6 +1,7 @@
 package com.example.buensaborback.domain.dto.articuloDto;
 
 import com.example.buensaborback.domain.dto.BaseDto;
+import com.example.buensaborback.domain.dto.UnidadMedidaDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,7 @@ public class ArticuloShortDto extends BaseDto {
     protected String denominacion;
     protected Double precioVenta;
     protected boolean habilitado;
+    protected UnidadMedidaDto unidadMedida;
     protected String codigo;
 
 }
